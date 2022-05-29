c0145c91db008912491d5b3a664a8c9fa38e4d61
Keshav <keshavjha018@gmail.com>
2022-05-30 00:55:10 +0530

    Footer Added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Links from './components/Links';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
       <Navbar />
       <Links />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./Footer.css"
+
+function Footer() {
+  return (
+    <>
+      <footer className='footer'>
+        <div className="ele-row">
+          <a href="https://github.com/keshavjha018/everlink" target="_blank" rel="noreferrer noopener"><i className="fa fa-github"></i></a>
+          <a href="https://www.linkedin.com/in/keshavjha018/" target="_blank" rel="noreferrer noopener"><i className="fa fa-linkedin"></i></a>
+        </div>
+        <div class="ele-row">
+          EVERLINK Copyright © 2022 - All rights reserved || Designed & Developed By: Keshav Jha 
+        </div>
+      </footer>
+    </>
+  )
+}
+
+export default Footer
