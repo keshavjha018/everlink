2fbf55de99da166b74e432afac34ef6a1391a92b
Keshav <keshavjha018@gmail.com>
2022-05-30 00:51:56 +0530

    Alignments fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -26,8 +26,8 @@ function Navbar() {
                             </li>
                         </ul>
                         <form className="d-flex">
-                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                                <button className="btn btn-outline-success" type="submit">Search</button>
+                            <input className="form-control me-2 search-input" type="search" placeholder="Search" aria-label="Search" />
+                                <button className="btn btn-outline-success search-btn" type="submit">Search</button>
                         </form>
                     </div>
                 </div>
